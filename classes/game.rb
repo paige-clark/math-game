fee8e19ae510d4e5a0109d8bd753a87bbea4e805
paige-clark <paigeclarkillustration@gmail.com>
2022-09-19 17:28:29 -0400

    Refactor play_game

--- classes/game.rb
+++ classes/game.rb
@@ -5,39 +5,28 @@ class Game
     @player1 = Player.new('Player 1')
     @player2 = Player.new('Player 2')
     @response = nil
-    # then we want to ask a question to the current player
+    @current_player = player1
   end
 
   def play_game
-    while player1.lives > 0 || player2.lives > 0
-      puts "----- NEW TURN -----"
-      @current_question = Question.new(player1)
+    while current_player.lives > 0
+      puts current_player.name
+      puts "\n----- NEW TURN -----"
+      @current_question = Question.new(current_player)
       @response = current_question.question
       if response == false
-        player1.lives -= 1
+        current_player.lives -= 1
         puts "P1: #{player1.lives}/3 P2: #{player2.lives}/3"
-
-        if player1.lives == 0
+        if current_player.lives == 0
           return puts "GAME OVER!!!"
         end
-
       else 
         puts "P1: #{player1.lives}/3 P2: #{player2.lives}/3"
       end
-
-      puts "----- NEW TURN -----"
-      @current_question = Question.new(player2)
-      @response = current_question.question
-      if response == false
-        player2.lives -= 1
-        puts "P1: #{player1.lives}/3 P2: #{player2.lives}/3"
-
-        if player1.lives == 0
-          return puts "GAME OVER!!!"
-        end
-
-      else 
-        puts "P1: #{player1.lives}/3 P2: #{player2.lives}/3"
+      if current_player.name == 'Player 1'
+        @current_player = player2
+      else
+        @current_player = player1
       end
     end
   end
--- classes/player.rb
+++ classes/player.rb
@@ -3,8 +3,8 @@ class Player
   def initialize(name)
     @name = name
     @lives = 3
-    puts "Hello from #{self.name}, I have #{self.lives}/3 lives remaining!"
+    # puts "Hello from #{self.name}, I have #{self.lives}/3 lives remaining!"
   end
 
-  
+
 end
